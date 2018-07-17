95a754e0057d6c303f06f25f3977e0381b189783
lukemuszynski <luke.muszynski@gmail.com>
2018-07-17 16:53:25 +0200

    Add files via upload

---
+++ RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Licensing.DataAccessLayer.Repositories.Abstracts.Base;
+
+namespace Licensing.DataAccessLayer.Repositories.Implementations.Base
+{
+    public abstract class RepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected TContext Context { get; private set; }
+
+        protected RepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        public virtual async Task<TEntity> Get(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
